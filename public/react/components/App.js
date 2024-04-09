a6fe6e2a53a676785c9cfc6f02074d18e537df4b
Alina Pervez <alpervez@deloitte.com>
2024-04-08 23:19:48 -0400

    feat: updated app.js with items component

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,33 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import { SaucesList } from './SaucesList';
+import { ItemsList } from './ItemsList';
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
 
 export const App = () => {
+  const [sauces, setSauces] = useState([]);
+  const [items, setItems] = useState([]);
 
-	const [sauces, setSauces] = useState([]);
+  async function fetchSauces(){
+    try {
+      const response = await fetch(`${apiURL}/sauces`);
+      const saucesData = await response.json();
+      setSauces(saucesData);
+    } catch (err) {
+      console.log("Oh no an error! ", err)
+    }
+  }
 
-	async function fetchSauces(){
-		try {
-			const response = await fetch(`${apiURL}/sauces`);
-			const saucesData = await response.json();
-			
-			setSauces(saucesData);
-		} catch (err) {
-			console.log("Oh no an error! ", err)
-		}
-	}
+  async function fetchItems(){
+    try {
+      const response = await fetch(`${apiURL}/items`);
+      const itemsData = await response.json();
+      setItems(itemsData);
+    } catch (err) {
+      console.log("Oh no an error!", err)
+    }
+  }
 
-	useEffect(() => {
-		fetchSauces();
-	}, []);
+  useEffect(() => {
+    fetchSauces();
+    fetchItems();
+  }, []);
 
-	return (
-		<main>	
+  return (
+    <main>
       <h1>Sauce Store</h1>
-			<h2>All things 🔥</h2>
-			<SaucesList sauces={sauces} />
-		</main>
-	)
+      <h2>All things 🔥</h2>
+      <SaucesList sauces={sauces} />
+      <h1>Items Store</h1>
+      <h2>All things</h2>
+      <ItemsList items={items} />
+    </main>
+  )
 }
--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -1,30 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import apiURL from '../api';
+import React from 'react';
+
 export const Item = (props) => {
-    const [item, setItem] = useState({})
 
-    async function fetchItem() {
-        try {
-            const response = await fetch(`${apiURL}/items/${props.???}`)
-            const data = await response.json()
-            setItem(data)
-        } catch (error) {
-            console.error('There was an error.', error)
-        }
-}
-useEffect(() => {
-    fetchItem()
-}, [])
-
-const[isOpen, setIsOpen] = useState(false);
-const handleClick = () =>  setIsOpen(!isOpen)
-return (
-    <div>
-        <h2>{item.name}</h2>
-        <p>{item.description}</p>
-        <p>{item.price}</p>
-        <button onClick={handleClick}>Toggle</button>
-        {isOpen && <p>{item.price}</p>}
-    </div>
-)
-}
+  return <>
+    <h3>{props.item.name}</h3>
+    <img src={props.item.image} alt={props.item.name} />
+  </>
+} 
+	
