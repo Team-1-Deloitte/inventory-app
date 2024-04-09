8e46e572f9e3e52e51ab6c35460c3aecc85f21fe
Liodice222 <iodice.pt@gmail.com>
2024-04-09 14:20:57 -0400

    saved items.js get individual and get all items

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -10,6 +10,7 @@ export const App = (props) => {
   const [sauces, setSauces] = useState([]);
   const [items, setItems] = useState([]);
   const [selectedItem, setSelectedItem] = useState(null);
+  const [newItem, setItem] = useState(null);
 
   async function fetchSauces(){
     try {
@@ -31,9 +32,20 @@ export const App = (props) => {
     }
   }
 
+  async function addItem() {
+    try {
+      const response = await fetch(`${apiURL}/items/:id`);
+      const newItem = await response.json();
+      setItem(newItem)
+    } catch(error) {
+      next(error)
+    }
+  }
+
   useEffect(() => {
     fetchSauces();
     fetchItems();
+    addItem();
   }, []);
 
  
