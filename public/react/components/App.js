7a088147000ac66dd7f36cf4a80795226d32b836
APDevTest <160549292+APDevTest@users.noreply.github.com>
2024-04-09 11:25:05 -0400

    Single item view (#13)
    * feat: Adding an onClick functionality
    
    * feat:Click to view each item

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { SaucesList } from './SaucesList';
 import { ItemsList } from './ItemsList';
+import { Item } from './Item';
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
 
-export const App = () => {
+export const App = (props) => {
   const [sauces, setSauces] = useState([]);
   const [items, setItems] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
 
   async function fetchSauces(){
     try {
@@ -34,14 +36,44 @@ export const App = () => {
     fetchItems();
   }, []);
 
+ 
+
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => setIsOpen(!isOpen);
+  console.log(props)
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+  };
+    
   return (
     <main>
-      <h1>Sauce Store</h1>
+      {/* <h1>Sauce Store</h1>
       <h2>All things 🔥</h2>
-      <SaucesList sauces={sauces} />
-      <h1>Items Store</h1>
-      <h2>All things</h2>
-      <ItemsList items={items} />
+      <SaucesList sauces={sauces} /> */}
+
+      <h1 onClick={handleClick}>Items Store</h1>
+      {isOpen && 
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <div>
+        <h4>Click on an item to see more details</h4>        
+          {items.map((item, index) => (
+          <p key={index} onClick={() => handleItemClick(item)}>{item.name}</p>
+          ))}
+        </div>
+        {selectedItem && 
+        <div>
+          <h4>Details:</h4>
+          <p>{selectedItem.name}</p>
+          <p>{selectedItem.category}</p>          
+          <p>{selectedItem.description}</p>
+          <p>${selectedItem.price}</p>
+          <img src={selectedItem.image} alt={selectedItem.name} width='200px' height='200'/>
+          {/* {Object.entries(selectedItem).map(([key, value]) => (
+            <p key={key}>{key}: {value}</p>
+          ))} */}
+          </div>}
+      </div>}
     </main>
   )
 }
