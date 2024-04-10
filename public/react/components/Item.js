d86809d106f4a62e61df9f402d9c71b90207d6c3
Aaliyah Plummer <aaliyahplummer826.gmail.com>
2024-04-09 22:47:05 -0700

    eighth commit. i fixed some errors along with the async and i added a button type at the bottom there. i will now push my code for review.

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -1,9 +1,9 @@
-import React, { useState}  from 'react';
+import React, { useState }  from 'react';
 
 export const Item = (props) => {
   const [newItem, setNewItem] = useState({ name: '', image: '' , body: '' });
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async(e) => {
     e.preventDefault(); 
     try{
       const response = await fetch('/api/items', {
@@ -24,7 +24,8 @@ export const Item = (props) => {
   }
  };
 
-  return <>
+  return ( 
+  <>
     <h3>{props.item.name}</h3>
     <img src={props.item.image} alt={props.item.name} />
     <button> Add Item </button>
@@ -48,7 +49,9 @@ export const Item = (props) => {
 
     <input type = {string} value = {props.item.body} ></input>
     <input type = "string" value={props.item.body}/>
-      
+
+    <button type="submit"> Add Item </button>
     </form>
   </>
+  );
 };
