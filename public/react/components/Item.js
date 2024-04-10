91abb39b91789cf4e5e9b3f7e7ccd8309f1a3ca7
Aaliyah Plummer <aaliyahplummer826.gmail.com>
2024-04-09 20:04:25 -0700

    second commit for fetch

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -2,9 +2,23 @@ import React, { useState}  from 'react';
 
 export const Item = (props) => {
   const [newItem, setNewItem] = useState({ name: '', image: '' , body: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    try{
+      const response = await fetch('/api/items', {
+        method: 'POST', 
+      headers:{'Content-Type': 'application/json',
+    }, body: JSON.stringify(newItem),
+    });
+
+    
+
+
   return <>
     <h3>{props.item.name}</h3>
     <img src={props.item.image} alt={props.item.name} />
   </>
 } 
-	
+};
+}
