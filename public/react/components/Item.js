dcae38a7c5edf1c32d1152cc7415acb79d55bc36
Aaliyah Plummer <aaliyahplummer826.gmail.com>
2024-04-09 19:27:21 -0700

    first part of the fetch

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState}  from 'react';
 
 export const Item = (props) => {
-
+  const [newItem, setNewItem] = useState({ name: '', image: '' , body: '' });
   return <>
     <h3>{props.item.name}</h3>
     <img src={props.item.image} alt={props.item.name} />
